af8311b2f141ad4b92346b061927661381d97d71
Bruno Boivin <bruno.boivin@mail.mcgill.ca>
2013-11-08 20:52:39 -0500

    Created StatisticsPkg and moved Statistics.java from GameControllerPkg to this new package.

---
+++ src/StatisticsPkg/Statistics.java
@@ -0,0 +1,27 @@
+package StatisticsPkg;
+
+
+public class Statistics 
+{
+
+  public String[][] getHighestScores() 
+  {
+  return null;
+  }
+
+  public boolean update(String playerA , String playerB, String winner) 
+  {
+  return false;
+  }
+
+  public int[] getPlayerStat( String playerA) 
+  {
+  return null;
+  }
+
+  public int[] getPairScores( String playerA,  String playerB) 
+  {
+  return null;
+  }
+
+}
