d2f0de2d9510c739437f9ae5faada2124d9caf8a
Bruno Boivin <bruno.boivin@mail.mcgill.ca>
2013-11-08 21:58:43 -0500

    Added Driver_Stats class to run and test the classes contained in StatisticsPkg. Created uml folder to store autogenerated uml diagrams.

---
+++ src/StatisticsPkg/Driver_Stats.java
@@ -0,0 +1,25 @@
+package StatisticsPkg;
+
+/** Class that runs and tests the other classes contained in the StatisticsPkg.
+ * 
+ * @author Bruno Boivin <bruno.boivin@mail.mcgill.ca>
+ * @version 1.0
+ * @since 2013-11-08
+ */
+public class Driver_Stats {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		long time0 = System.currentTimeMillis();
+		//******TIME-BEG
+		
+		
+		//******TIME-END
+		System.out.println();
+		System.out.println(System.currentTimeMillis() - time0 + " ms");
+	}
+
+}
