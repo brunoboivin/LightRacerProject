1415bd62d08c77929ce6c0388eaea5bf1ef55f91
Bruno Boivin <bruno.boivin@mail.mcgill.ca>
2013-11-08 22:55:00 -0500

    Added StatisticsFile Enum to list all files used in StatisticsPkg. Filename and path can be retrieved using the defined enum getters.

---
+++ src/StatisticsPkg/StatisticsFiles.java
@@ -0,0 +1,39 @@
+package StatisticsPkg;
+
+/** Enum used to list all files (filename and path) used in StatisticsPkg.
+ * 
+ * @author Bruno Boivin <bruno.boivin@mail.mcgill.ca>
+ * @version 1.0
+ * @since 2013-11-08
+ */
+public enum StatisticsFiles {
+	PLAYER_RECORDS("player_records.csv"),
+	PAIR_RECORDS("pair_records.csv");
+	
+	private String filename;
+	private String path;
+	
+	/**
+	 * Constructor
+	 * @param filename
+	 */
+	StatisticsFiles (String filename){
+		this.filename = filename;
+		this.path = "csv/" + filename;
+	}
+	
+	//Getters
+	/**
+	 * @return filename
+	 */
+	public String getFilename(){
+		return filename;
+	}
+	
+	/**
+	 * @return path to file
+	 */
+	public String getPath(){
+		return path;
+	}
+}
