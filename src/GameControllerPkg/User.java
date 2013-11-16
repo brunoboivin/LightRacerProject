577b6d49c7b72ae4353721e4b65c674199653a08
Shahrzad <Shahrzad@Sugar>
2013-11-16 14:46:17 -0500

    Created the User class based on requirements.

---
+++ src/GameControllerPkg/User.java
@@ -0,0 +1,38 @@
+package GameControllerPkg;
+
+public class User 
+{
+	private String username;
+	private String password;
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
