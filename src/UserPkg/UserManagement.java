169ae836fd2e80792c7e8801b40eac1976e820e4
anitaszilagyi <anitaszilagyi@vpn164252.wireless.mcgill.ca>
2013-11-16 19:54:05 -0500

    users

---
+++ src/UserPkg/UserManagement.java
@@ -0,0 +1,26 @@
+package UserPkg;
+
+import RacerPkg.*;
+import GridPkg.*;
+
+public class UserManagement 
+{
+
+  //  public myUIController;
+
+  public Racer login( String username ,  String password ) 
+  {
+	  
+  return null;
+  }
+
+  public void logout( String username) 
+  {
+  }
+
+  public boolean registerUser() 
+  {
+  return false;
+  }
+
+}
