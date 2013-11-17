1f5e29910c08ba9fcda45d4042df77130d168a58
anitaszilagyi <anitaszilagyi@vpn164252.wireless.mcgill.ca>
2013-11-16 20:45:42 -0500

    .

--- src/GameControllerPkg/UIController.java
+++ src/GameControllerPkg/UIController.java
@@ -1,15 +1,24 @@
 package GameControllerPkg;
 
 import RacerPkg.*;
+import UserPkg.User;
 import GridPkg.*;
 
 
 
 public class UIController {
 
-  public Racer playerA;
-
-  public Racer playerB;
+//  public static User userA;
+//
+//  public static User userB;
+//  
+//  public static void setUser(User user) {
+//	  if (userA.getUsername().isEmpty()) {
+//		  userA = user;
+//	  } else {
+//		  userB = user;
+//	  }
+//  }
 
   private Grid currentMap;
 
--- src/UserPkg/UserManagement.java
+++ src/UserPkg/UserManagement.java
@@ -1,26 +1,54 @@
 package UserPkg;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import RacerPkg.*;
+import StatisticsPkg.PairRecord;
+import CSVPkg.CSVHandler;
+import GameControllerPkg.UIController;
 import GridPkg.*;
 
 public class UserManagement 
 {
 
-  //  public myUIController;
+	//  public myUIController;
 
-  public Racer login( String username ,  String password ) 
-  {
-	  
-  return null;
-  }
+	public boolean login( String username, String password) 
+	{
+		User newUser = new User(username, password);
 
-  public void logout( String username) 
-  {
-  }
+		ArrayList<User> users = new ArrayList<User>();
+		try {
+			users = (ArrayList<User>) CSVHandler.read("csv/user_data.csv");
+		} catch (IOException e) {
+			return false;
+		}
+		//users.add(newUser);
+		for (User user : users) {
+			if ((user.getUsername()).equals(username)) {
+				if (user.getPassword().equals(password)) {
+					UIController.setUser(newUser);
+				}
+			}
+		}
+		return true;
+	}
 
-  public boolean registerUser() 
-  {
-  return false;
-  }
+	public void logout( String username) 
+	{
+	}
+
+	public boolean registerUser() 
+	{
+		// read the csv to get the array list
+		User newUser = new User(username, password);
+		ArrayList<User> users = new ArrayList<User>();
+		// update that
+
+		// write the array list back
+
+		return false;
+	}
 
 }
