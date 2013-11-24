e1399d355855a757bbab09eb1e96af6da8257851
brunoboivin <bruno.boivin@mail.mcgill.ca>
2013-11-24 17:44:22 -0500

    Created TestPkg. Added unit tests for some of the classes in the Statistics package.

---
+++ src/TestPkg/Test_Statistics.java
@@ -0,0 +1,14 @@
+package TestPkg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Statistics {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
