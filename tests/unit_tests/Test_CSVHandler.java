8bcf58dc7d56271d071726aff81a39b71acc2e19
brunoboivin <bruno.boivin@mail.mcgill.ca>
2013-11-25 00:24:09 -0500

    Added unit tests for CSVHandler class.

---
+++ tests/unit_tests/Test_CSVHandler.java
@@ -0,0 +1,121 @@
+package unit_tests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import CSVPkg.CSVHandler;
+import StatisticsPkg.PlayerRecord;
+import UserPkg.User;
+
+public class Test_CSVHandler {
+
+	String path = "tests/unit_tests/tmp.csv";
+	File file = new File(path);
+	
+	@Before
+	public void setUp() throws Exception {
+		file.createNewFile();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		file.delete();
+	}
+
+	@Test
+	public void test_write_ArrayList() {
+		long empty = file.length();
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		PlayerRecord record = new PlayerRecord("John Smith");
+		User user = new User("John", "Smith");
+		list.add(record);
+		list.add(user);
+		
+		CSVHandler.write(list, path);
+		assertTrue(empty != file.length());
+	}
+	
+	@Test
+	public void test_write_HashMap() {
+		long empty = file.length();
+		Hashtable<Object, Object> map = new Hashtable<Object, Object>();
+		
+		PlayerRecord record = new PlayerRecord("John Smith");
+		User user = new User("John", "Smith");
+		map.put("record", record);
+		map.put("user", user);
+		
+		CSVHandler.write(map, path);
+		assertTrue(empty != file.length());
+	}
+
+	@Test
+	public void test_read_ArrayList() {
+		//write to file
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		PlayerRecord record = new PlayerRecord("John Smith");
+		User user = new User("John", "Smith");
+		list.add(record);
+		list.add(user);
+		
+		CSVHandler.write(list, path);
+		
+		//read from file
+		ArrayList<Object> listRead = (ArrayList<Object>) CSVHandler.read(path);
+		
+		//verify data read is the same as data written to the file
+		assertTrue(list.size() == listRead.size());
+		
+		PlayerRecord recordRead = (PlayerRecord) listRead.get(0);
+		User userRead = (User) listRead.get(1);
+		
+		assertTrue( record.getUsername().equals(recordRead.getUsername()) );
+		assertTrue( record.getGamesWon() == recordRead.getGamesWon() );
+		assertTrue( record.getGamesPlayed() == recordRead.getGamesPlayed() );
+		
+		assertTrue( user.loggedIn == userRead.loggedIn );
+		assertTrue( user.password.equals(userRead.password));
+		assertTrue( user.username.equals(userRead.username));
+	}
+	
+	@Test
+	public void test_read_HashMap() {
+		//write to file
+		Hashtable<Object, Object> map = new Hashtable<Object, Object>();
+		
+		PlayerRecord record = new PlayerRecord("John Smith");
+		User user = new User("John", "Smith");
+		map.put("record", record);
+		map.put("user", user);
+		
+		CSVHandler.write(map, path);
+
+		//read from file
+		Hashtable<Object, Object> mapRead = (Hashtable<Object, Object>) CSVHandler.read(path);
+		
+		//verify data read is the same as data written to the file
+		assertTrue(map.keySet().size() == mapRead.keySet().size());
+		
+		PlayerRecord recordRead = (PlayerRecord)mapRead.get("record");
+		User userRead = (User)mapRead.get("user");
+		
+		assertTrue( record.getUsername().equals(recordRead.getUsername()) );
+		assertTrue( record.getGamesPlayed() == recordRead.getGamesPlayed() );
+		assertTrue( record.getGamesWon() == recordRead.getGamesWon() );
+		
+		assertTrue( user.loggedIn == userRead.loggedIn );
+		assertTrue( user.password.equals(userRead.password) );
+		assertTrue( user.username.equals(userRead.username) );
+	}
+}
