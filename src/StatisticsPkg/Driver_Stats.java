63b599ae1319e253314fb79c802f7916a095e7c2
Kaichen Wang <kaichen.wang@outlook.com>
2013-11-24 19:06:22 -0500

    Turned back time to fix CSVs

--- src/StatisticsPkg/Driver_Stats.java
+++ src/StatisticsPkg/Driver_Stats.java
@@ -25,13 +25,13 @@ class Driver_Stats {
 
 		
 		//REINITIALIZES BOTH CSV FILES
-		/*
+		
 		Hashtable<String, PlayerRecord> playerRecords = new Hashtable<String, PlayerRecord>();
 		ArrayList<PairRecord> pairRecords = new ArrayList<PairRecord>();
 
 		CSVHandler.write(playerRecords, StatisticsFiles.PLAYER_RECORDS.getPath());
 		CSVHandler.write(pairRecords, StatisticsFiles.PAIR_RECORDS.getPath());
-		*/
+		
 		 
 		//PRINT USERNAMES/PASSWORDS
 		ArrayList<User> users = (ArrayList<User>) CSVHandler.read("csv/user_data.csv");
