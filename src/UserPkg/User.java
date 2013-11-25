1c8fec82230f381b5c18b395c397a65076281892
ShahrzadT <Shahrzad@142.157.150.254>
2013-11-24 21:24:19 -0500

    fixed the User class. // there were unwanted and duplicate lines

--- src/UserPkg/User.java
+++ src/UserPkg/User.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package UserPkg;
 
 import java.io.Serializable;
@@ -27,46 +26,4 @@ public class User implements Serializable
 
 
 }
-=======
-package UserPkg;
 
-import java.io.Serializable;
-
-public class User implements Serializable
-{
-	public String username;
-	public String password;
-	public boolean loggedIn;
-	
-	public User(String username,String password)
-	{
-		this.username = username;
-		this.password = password;
-	}
-	
-	public void logIn (User user) {
-		user.loggedIn = true;
-	}
-	
-	public void logOut (User user) {
-		user.loggedIn = false;
-	}
-
-	public boolean isLoggedIn(User user) {
-		return user.loggedIn;
-	}
-	
-	public String getUsername() 
-	{
-		return username;
-	}
-
-	public String getPassword() 
-	{
-		return password;
-	}
-
-
-
-}
->>>>>>> b56249abee613c53a7c2f36bb227cd7aeca8cd80
