c87fd3a32cb7265aae2fcb7e601e257c84eadd26
Kaichen Wang <kaichen.wang@outlook.com>
2013-11-29 04:04:12 -0500

    JavaDoc comments added to GridCell class

--- src/GridPkg/GridCell.java
+++ src/GridPkg/GridCell.java
@@ -1,5 +1,11 @@
 package GridPkg;
 
+/**
+ * 
+ * @author Kaichen
+ * Enum for each possible state of an individual cell of a Grid
+ * (Empty, Obstacle, Icon1:Yoda, Icon2:DarthVader, Color1:Green, Color2:Red)
+ */
 public enum GridCell 
 {
 
