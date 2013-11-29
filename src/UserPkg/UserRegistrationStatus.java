7867a22a2a255025dfd52977708b1eb1f013cb13
anitaszilagyi <anitaszilagyi@aszilagyi.local>
2013-11-29 13:00:25 -0500

    Javadoc for UserPkg

--- src/UserPkg/UserRegistrationStatus.java
+++ src/UserPkg/UserRegistrationStatus.java
@@ -1,22 +1,29 @@
 package UserPkg;
 
+/**
+ * Enumeration for the status of the user's registration attempt.
+ * 
+ * @author Anita Szilagyi <anita.szilagyi@mail.mcgill.ca>
+ * @version 1.0
+ * @since 2013-11-08
+ */
 public enum UserRegistrationStatus {
-	
+
 	/**
 	 * Successful registration.
 	 */
 	Success,
-	
+
 	/**
 	 * Unsuccessful registration: username taken.
 	 */
 	UsernameTaken,
-	
+
 	/**
 	 * Unsuccessful registration: password does not meet requirements.
 	 */
 	BadPassword,
-	
+
 	/**
 	 * Error writing/reading to/from the user_data.csv file.
 	 */
